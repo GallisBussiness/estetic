ed821b897b80c580f84a8bba2a66e627809c07b5
Mamadou Sy <bussinessgallis@gmail.com>
2021-11-06 00:34:18 +0000

    new update to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,7 +31,7 @@ function Home() {
   <div className="container mx-auto flex px-24 py-10 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 className="sm:text-5xl mb-4 font-semibold font-lora text-gray-900">Bienvenue chez Kari's beauty center</h1>
-      <p className="mb-8 leading-relaxed font-lora"> CK ESTHETIK se veut un centre d’excellence au service de sa clientèle, c’est dans cette optique qu’il vous
+      <p className="mb-8 leading-relaxed font-lora text-gray-800"> CK ESTHETIK se veut un centre d’excellence au service de sa clientèle, c’est dans cette optique qu’il vous
 propose une gamme très variée de soins du corps et visage ainsi que des soins des mains et des pieds, des
 massages, pour un public masculin et féminin, maquillage et bien d’autres prestations. </p>
       <div className="flex justify-center">
